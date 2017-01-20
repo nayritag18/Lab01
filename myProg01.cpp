5dcce40f3864c80fb35d6437dcddf25a93452e0d
nayritag18 <ntagmazian@umail.ucsb.edu>
2017-01-19 18:11:12 -0800

    Add files via upload

---
+++ myProg01.cpp
@@ -0,0 +1,17 @@
+// myProg01.cpp
+// Author: Nayri Tagmazian
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    // Simple that counts down from n to 1
+ 
+    int n=5;
+ 
+    for (int i=n; i>0; i--) {
+       cout << i << " ";
+    }
+    cout << endl; 
+    return 0;
+}
+
